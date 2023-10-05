5602dd1c8d3366313c8e8d79366b9bd1df6e89c1
Md Razwan Niam <razwanniam1@gmail.com>
2023-10-05 23:20:35 +0600

    all programms performed

--- shopping.c
+++ shopping.c
@@ -3,7 +3,14 @@ int main()
 {
     int a;
     scanf("%d", &a);
-    if(a>=1500){
-         
+    if (a >= 1500)
+    {
+        printf("I will buy Punjabi\nI will buy new shoes\nAlisa will buy new shoes");
+    }
+    else if (a > 1000)
+        printf("I will buy Punjabi");
+    else
+    {
+        printf("Bad luck!");
     }
 }
